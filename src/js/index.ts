7a09140d26de903ea54bed0ad66a2705f12155d6
Nico Engelhardt <nico.engelhardt@otto.de>
2020-08-18 16:27:05 +0200

    fix(ui): cleanup to top link in logo

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,21 @@
-function initRuleFilter(): void {
-  // eslint-disable-next-line no-console
-  console.log("Implement Me");
+function logoLinkToTop(): void {
+  const logoElement: HTMLLinkElement | null = document.querySelector(
+    ".js_api-sidebar__logo-link"
+  );
+
+  if (logoElement) {
+    logoElement.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.documentElement.scrollTop = 0;
+      window.history.pushState(
+        "",
+        document.title,
+        window.location.pathname + window.location.search
+      );
+    });
+  }
 }
 
-initRuleFilter();
+document.addEventListener("DOMContentLoaded", function () {
+  logoLinkToTop();
+});
