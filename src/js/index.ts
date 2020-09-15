2063b240ab19a5b87dba7334e7e5fa2751bcba56
Nico Engelhardt <39621217+nicoengelhardt0777@users.noreply.github.com>
2020-09-15 10:52:39 +0200

    feat(ui): add rule filters (#348)
    * add sticky navbar including filter dropdown
    * toggle css class on select change
    * adjust selectbox options
    * scroll to top and blur selectbox on filter select
    * add display style rules

--- src/js/index.ts
+++ src/js/index.ts
@@ -16,6 +16,32 @@ function logoLinkToTop(): void {
   }
 }
 
+function initRuleFilter(): void {
+  const ruleSelection = document.querySelectorAll(
+    ".js_api-rule-selection"
+  )[0] as HTMLSelectElement;
+  const cssClasses = [...ruleSelection.options]
+    .map((option: HTMLOptionElement) => option.value)
+    .filter((x, i, a) => a.indexOf(x) === i)
+    .filter((x) => x !== "");
+  const apiContainer: Element = document.querySelectorAll(".api-container")[0];
+
+  ruleSelection.addEventListener("change", (event: Event) => {
+    const target = event.target as HTMLSelectElement;
+
+    apiContainer.classList.remove(
+      ...cssClasses.map((cl) => `api-rule-filter-${cl}`)
+    );
+    if (target.value !== "") {
+      apiContainer.classList.add(`api-rule-filter-${target.value}`);
+    }
+
+    document.documentElement.scrollTo(0, 0);
+    target.blur();
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   logoLinkToTop();
+  initRuleFilter();
 });
