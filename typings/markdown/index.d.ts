77a2bbf16acecb42b9c02914b1a4db0da3eb7d66
Tino Prahl <tino.prahl@otto.de>
2022-01-20 08:31:41 +0100

    chore(code): clean up

--- typings/markdown/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "markdown-it-attrs";
-declare module "markdown-it-container";
-declare module "markdown-it-front-matter";
-declare module "markdown-it-highlightjs";
-declare module "markdown-it-footnote";
