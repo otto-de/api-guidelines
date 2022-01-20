7e086074063f681878137fd07e72e420fdbcfaea
Tino Prahl <tino.prahl@otto.de>
2022-01-20 10:58:56 +0100

    fix(pipeline): enable some cli commands again

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,2 +1,4 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import "@otto-ec/assets-core-utils";
+
+import "@otto-ec/ottoapi-portal-guidelines/src/plugins";
