80b27188d23c919093f252f6286e2065dd01de85
Tino Prahl <tino.prahl@otto.de>
2022-01-27 08:22:26 +0100

    npm(priv): update portal-common and portal-guidelines

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import "@otto-ec/assets-core-utils";
 
+import "@otto-ec/ottoapi-portal-common/src/types/markdown";
 import "@otto-ec/ottoapi-portal-guidelines/src/plugins";
