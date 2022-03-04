ed23df962080bdf3e410928656532bb2a0f23834
Tino Prahl <tino.prahl@otto.de>
2022-03-04 13:35:03 +0100

    npm(priv): update portal-common and portal-guidelines

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "@otto-ec/ottoapi-portal-common/vite.config";
 import { guidelines } from "@otto-ec/ottoapi-portal-guidelines/vite.config";
+import { join } from "path";
 
-export default defineConfig({ outDir: "tmp/viteout" }, guidelines);
+export default defineConfig(
+  { outDir: "tmp/viteout" },
+  guidelines({
+    path: join(process.cwd(), "content", "guidelines"),
+  })
+);
