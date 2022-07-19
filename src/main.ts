678ac3016252d7df5befe1cc39514842ff801dbc
Tino Prahl <tino.prahl@otto.de>
2022-07-19 08:09:25 +0200

    chore(portal): update portal deps

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ import "vuex";
 import "vue-router";
 
 import { createDefaultApp } from "@otto-ec/ottoapi-portal-common";
-import guidelines from "@otto-ec/ottoapi-portal-guidelines";
+import guidelines, {
+  definedTestingTopNav,
+  definedTestingSideNav,
+} from "@otto-ec/ottoapi-portal-guidelines";
 
 /** This is used to add missing "/" route to the dev app" */
 guidelines.routes = [...guidelines.routes, { path: "/", name: "home", redirect: "/guidelines" }];
 
-createDefaultApp({ modules: [guidelines] });
+createDefaultApp({
+  modules: [guidelines],
+  definedSideNav: definedTestingSideNav,
+  definedTopNav: definedTestingTopNav,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import { guidelines } from "@otto-ec/ottoapi-portal-guidelines/vite.config";
 import { join } from "path";
 
 export default defineConfig(
-  { outDir: "tmp/viteout" },
+  {
+    outDir: "tmp/viteout",
+    topNavConfigPath: "./node_modules/@otto-ec/ottoapi-portal-guidelines/src/testing/topNav.ts",
+    sideNavConfigPath: "./node_modules/@otto-ec/ottoapi-portal-guidelines/src/testing/sideNav.ts",
+  },
   guidelines({
     path: join(process.cwd(), "content", "guidelines"),
     changelogPath: join(process.cwd(), "changes"),
