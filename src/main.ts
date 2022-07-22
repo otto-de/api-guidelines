ee5f0a707bdbe05ba1caac9a5dc7a3cb49e87c7b
Tino Prahl <tino.prahl@otto.de>
2022-07-22 14:54:41 +0200

    npm(priv): update guidelines

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,17 @@ import "vuex";
 import "vue-router";
 
 import { createDefaultApp } from "@otto-ec/ottoapi-portal-common";
-import guidelines, {
-  definedTestingTopNav,
-  definedTestingSideNav,
-} from "@otto-ec/ottoapi-portal-guidelines";
+import guidelines, { testing } from "@otto-ec/ottoapi-portal-guidelines";
 
 /** This is used to add missing "/" route to the dev app" */
 guidelines.routes = [...guidelines.routes, { path: "/", name: "home", redirect: "/guidelines" }];
 
-createDefaultApp({
-  modules: [guidelines],
-  definedSideNav: definedTestingSideNav,
-  definedTopNav: definedTestingTopNav,
-});
+(async () => {
+  const { definedTestingTopNav, definedTestingSideNav } = await testing();
+
+  createDefaultApp({
+    modules: [guidelines],
+    definedSideNav: definedTestingSideNav,
+    definedTopNav: definedTestingTopNav,
+  });
+})();
