fa7b76d4b6e3a0476f67b6bdeed92b7c0d85df76
Tino Prahl <tino.prahl@otto.de>
2022-10-20 07:16:34 +0200

    chore(bender): fix configs to work with bender@next

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,15 +1,5 @@
-
-// MANAGED BY: @otto-ec/bender
-// remove magic string if you want to override default config
-
-/** @type {import("@commitlint/types").UserConfig} */
-const config = {
-  ...require("@otto-ec/bender/configs/.commitlintrc"),
-  // Extrend config here if needed
-  // rules: {
-  //   "footer-empty": [2, "always"]
-  // }
-};
-
-module.exports = config;
-
+module.exports = {
+  rules: {
+    "scope-case": [2, "always", "lower-case"]
+  }
+}
