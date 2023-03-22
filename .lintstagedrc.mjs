bec9d9db64b57eec41d6dbe9e840fcc06acb14d3
Tino Prahl <tino.prahl@otto.de>
2023-03-22 10:37:54 +0100

    chore: markdownlint should fix files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
   "*.ts": ["prettier --write", "eslint --fix"],
-  "*.md": ["prettier --write", "markdownlint"],
+  "*.md": ["prettier --write", "markdownlint --fix"],
   "*.yml": ["prettier --write"],
 };
