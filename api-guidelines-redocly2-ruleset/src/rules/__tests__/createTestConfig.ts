dc7f395281f0186d73902546639667ab78940d7d
Max Edenharter <max.edenharter@otto.de>
2026-04-20 11:20:50 +0200

    [OTTOAPI-1601] Refactor createTestConfig.ts

--- api-guidelines-redocly2-ruleset/src/rules/__tests__/createTestConfig.ts
+++ api-guidelines-redocly2-ruleset/src/rules/__tests__/createTestConfig.ts
@@ -1,24 +1,31 @@
-import { createConfig, Config } from "@redocly/openapi-core";
+import {
+    createConfig,
+    type Config,
+    type Plugin,
+    type RuleConfig,
+    type RawUniversalConfig,
+    type Oas3Rule,
+    type Oas2Rule,
+    type Async2Rule,
+    type Async3Rule,
+} from "@redocly/openapi-core";
 
-type CustomRulesConfig = {
-  oas3?: Record<string, unknown>;
-  oas2?: Record<string, unknown>;
-};
+type AnyRule = Oas3Rule | Oas2Rule | Async2Rule | Async3Rule;
+
+
+export type CustomRulesConfig = Partial<Record<keyof NonNullable<Plugin["rules"]>, Record<string, AnyRule>>>;
 
 export async function createTestConfig(customRulesConfig: CustomRulesConfig): Promise<Config> {
-  return createConfig({
-    plugins: [
-      {
-        id: "test-plugin",
-        // @ts-ignore
-        rules: customRulesConfig,
-      },
-    ],
-    // @ts-ignore
-    rules: Object.fromEntries(
-      Object.values(customRulesConfig).flatMap((ruleSet) =>
-        Object.keys(ruleSet).map((rule) => [`test-plugin/${rule}`, "error"]),
-      ),
-    ),
-  });
+    const pluginId = "test-plugin";
+
+    const prefixedRules = Object.fromEntries(
+        Object.values(customRulesConfig).flatMap((ruleSet) =>
+            Object.keys(ruleSet ?? {}).map((rule) => [`${pluginId}/${rule}`, "error" as RuleConfig]),
+        ),
+    );
+
+    return createConfig({
+        plugins: [{ id: pluginId, rules: customRulesConfig as NonNullable<Plugin["rules"]> }],
+        rules: prefixedRules,
+    } as RawUniversalConfig);
 }
